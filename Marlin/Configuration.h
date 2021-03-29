f383010f27c0a5997f7a6c07cb130385105c5390
Federico Romero <federom@gmail.com>
2021-03-29 15:15:20 -0300

    Bed boundary adjustments

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -990,7 +990,7 @@
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 20
+#define PROBING_MARGIN 10
 
 // X and Y axis travel speed (mm/min) between probes
 #define XY_PROBE_SPEED (133*60)
@@ -1126,8 +1126,8 @@
 #define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -20
-#define Y_MIN_POS -15
+#define X_MIN_POS -35
+#define Y_MIN_POS -19
 #define Z_MIN_POS 0
 #define X_MAX_POS 245
 #define Y_MAX_POS 220
